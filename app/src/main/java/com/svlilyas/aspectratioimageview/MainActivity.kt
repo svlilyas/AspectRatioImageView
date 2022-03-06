cc96a6f1a95cf0974694fc37c6d77199afb42e0f
svlilyas <ilyas.savli47@gmail.com>
2022-03-06 21:12:56 +0300

    Changes
    Some Changes.

--- app/src/main/java/com/svlilyas/aspectratioimageview/MainActivity.kt
+++ app/src/main/java/com/svlilyas/aspectratioimageview/MainActivity.kt
@@ -2,7 +2,6 @@ package com.svlilyas.aspectratioimageview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.svlilyas.aspectratioimageview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
